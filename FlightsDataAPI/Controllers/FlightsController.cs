b3ac6c3d3b666c528587050556674d2d4885dbc7
Santiago Sica <santisica29@gmail.com>
2026-03-13 21:43:41 -0300

    create controller class

---
+++ FlightsDataAPI/Controllers/FlightsController.cs
@@ -0,0 +1,15 @@
+﻿using FlightsDataAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightsDataAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FlightsController : ControllerBase
+{
+    private readonly IFlightService _flightService;
+    public FlightsController(IFlightService flightService)
+    {
+        _flightService = flightService;
+    }
+}
