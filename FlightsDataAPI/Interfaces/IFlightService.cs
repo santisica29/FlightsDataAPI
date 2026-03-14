5ad4c596f9dd40a304ddac1c888346793eed181f
Santiago Sica <santisica29@gmail.com>
2026-03-13 21:15:44 -0300

    implement interface for service class

--- FlightsDataAPI/Interfaces/IFlightService.cs
+++ FlightsDataAPI/Interfaces/IFlightService.cs
@@ -6,6 +6,6 @@ public interface IFlightService
     public List<Flight> GetAllFlights();
     public Flight? GetFlightById(int id);
     public Flight CreateFlight(Flight flight);
-    public Flight UpdateFlight(int id, Flight updatedFlight);
+    public Flight? UpdateFlight(int id, Flight updatedFlight);
     public string? DeleteFlight(int id);
 }
--- FlightsDataAPI/Services/FlightService.cs
+++ FlightsDataAPI/Services/FlightService.cs
@@ -18,4 +18,41 @@ public class FlightService : IFlightService
         _dbContext.SaveChanges();
         return savedFlight.Entity;
     }
+
+    public string? DeleteFlight(int id)
+    {
+        Flight? savedFlight = GetFlightById(id);
+
+        if (savedFlight == null)
+            return null;
+
+        _dbContext.Flights.Remove(savedFlight);
+        _dbContext.SaveChanges();
+
+        return $"Successfully deleted flight with id: {id}";
+    }
+
+    public List<Flight> GetAllFlights()
+    {
+        return _dbContext.Flights.ToList();
+    }
+
+    public Flight? GetFlightById(int id)
+    {
+        Flight? savedFlight = _dbContext.Flights.Find(id);
+        return savedFlight;
+    }
+
+    public Flight? UpdateFlight(int id, Flight flight)
+    {
+        Flight? savedFlight = GetFlightById(id);
+
+        if (savedFlight == null)
+            return null;
+
+        _dbContext.Entry(savedFlight).CurrentValues.SetValues(flight);
+        _dbContext.SaveChanges();
+
+        return savedFlight;
+    }
 }
